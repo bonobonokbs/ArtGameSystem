03b5bbcc0284f8b4f0b509135bb4a2997e3bc0f8
bonobonokbs <dhtpals32123@Naver.om>
2019-11-18 05:12:37 +0900

    충돌감지 테스트

--- sketch.js
+++ sketch.js
@@ -4,21 +4,22 @@ var box;
 
 function setup() {
   createCanvas(800, 400);
-  box = createSprite(100, 300);
+  box = createSprite(50, 10, 100,400);
 
 }
 
 function draw() {
   background(255, 255, 255);
 
-  fill(255,0,0);
-
   fill(0);
   textAlign(CENTER);
   text('Click to create a new sprite', width/2, height/2);
-  //draw all the sprites added to the sketch so far
-  //the positions will be updated automatically at every cycle
   drawSprites();
+
+  if(box.collide(s)){
+      print("충돌됨");
+  }
+
 }
 
 function mousePressed() {
@@ -27,6 +28,7 @@ function mousePressed() {
   var s = createSprite(mouseX, mouseY, 30, 30);
   //if no image or animation is associated it will be a rectancle of the specified size
   //and a random color
+
 
   //now you can use the variable to set properties
   //e.g. a random velocity on the x and y coordinates

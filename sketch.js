c136035a51a2b4e01ed8aba7376a3767ed40c434
bonobonokbs <dhtpals32123@Naver.om>
2019-11-18 10:11:10 +0900

    예제를 불러왔는데 그 사진은 못불러와서 일단 네모로

--- sketch.js
+++ sketch.js
@@ -1,33 +1,69 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
+//mouse states and mouse events on sprites
+//click and hold the mouse button while overing on the sprites
+
+var asterisk;
+var ghost;
+var draggedSprite;
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  createCanvas(800, 400);
+
+  ghost = createSprite(200, 200);
+  //ghost.addAnimation('normal', 'assets/ghost_spin0001.png', 'assets/ghost_spin0003.png');
+  //detect the mouse position and click on this sprite
+  //if no collider is defined, the image bounding box will be checked
+  ghost.mouseActive = true;
+
+  asterisk = createSprite(600, 200);
+  //asterisk.addAnimation('normal', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
+  //asterisk.addAnimation('stretch', 'assets/asterisk_stretching0001.png', 'assets/asterisk_stretching0008.png');
+  //var anim = asterisk.addAnimation('transform', 'assets/asterisk_circle0001.png', 'assets/asterisk_circle0008.png');
+
+  //if defined, the collider will be used for mouse events
+  asterisk.setCollider('circle', 0, 0, 64);
+
+  //I can assign functions to be called upon specific mouse events
+  //within the function "this" will reference the sprite that triggered the event
+  asterisk.onMouseOver = function() {
+    this.changeAnimation('stretch');
+  };
+
+  asterisk.onMouseOut = function() {
+    this.changeAnimation('normal');
+  };
+
+  asterisk.onMousePressed = function() {
+    this.changeAnimation('transform');
+    this.animation.goToFrame(this.animation.getLastFrame());
+    if (draggedSprite == null) {
+      draggedSprite = this;
+    }
+  };
+
+  asterisk.onMouseReleased = function() {
+    this.changeAnimation('transform');
+    this.animation.goToFrame(0);
+    if (draggedSprite == this) {
+      draggedSprite = null;
+    }
+  };
+
 }
 
 function draw() {
   background(255, 255, 255);
 
- fill(0);
- textAlign(CENTER);
- text('Click to create a new sprite', width/2, height/2);
- //draw all the sprites added to the sketch so far
- //the positions will be updated automatically at every cycle
- drawSprites();
-}
+  if (draggedSprite != null) {
+    draggedSprite.position.x = mouseX;
+    draggedSprite.position.y = mouseY;
+  }
 
-function mousePressed() {
+  //if a sprite is mouseActive true I can check if the mouse is over its collider
+  //and if the button is pressed
+  if(ghost.mouseIsOver)
+    ghost.rotation-= 10;
 
-  //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
-  //if no image or animation is associated it will be a rectancle of the specified size
-  //and a random color
+  ghost.visible = !ghost.mouseIsPressed;
 
-  //now you can use the variable to set properties
-  //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
+  drawSprites();
 }

331fca9015668a723659e4251a6ab75978b5342d
bonobonokbs <dhtpals32123@Naver.om>
2019-11-25 17:10:20 +0900

    11

--- sketch.js
+++ sketch.js
@@ -1,33 +1,89 @@
+let frog;
+let car1;
+let goal;
+let sound_hit;
+//------------
 let canvas;
-let canvasWidth = 600;
+let canvasWidth = 400;
 let canvasHeight = 400;
+
+function preload() {
+  sound_hit = loadSound('hit.wav'); // 경로 넣고 싶으면 'wave/hit.wav'
+}
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+
+
+  resetGame();
+
 }
 
 function draw() {
-  background(255, 255, 255);
+  background(220);
 
- fill(0);
- textAlign(CENTER);
- text('Click to create a new sprite', width/2, height/2);
- //draw all the sprites added to the sketch so far
- //the positions will be updated automatically at every cycle
- drawSprites();
+
+  if (car1.position.x >= width) {
+    car1.position.x = 0;
+    car1.setVelocity(random(3, 10), 0);
+  }
+
+
+
+  if (frog.bounce(car1)) {
+    sound_hit.play();
+  }
+
+  // 충돌 시 사운드 효과에 대한 또 다른 방법
+  // frog.collide(car1, playHitSound);
+  // frog.bounce(car1);
+
+
+  if (frog.overlap(goal)) {
+    nextLevel();
+  }
+
+  drawSprites();
+  checkGameOver();
+
 }
 
-function mousePressed() {
 
-  //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
-  //if no image or animation is associated it will be a rectancle of the specified size
-  //and a random color
+function resetGame() {
+  frog = createSprite(width/2, height-30, 20, 40);
+  goal = createSprite(width/2, 0, width, 4);
+  car1 = createSprite(0, height/2, 60, 30);
 
-  //now you can use the variable to set properties
-  //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
+  car1.setVelocity(random(3, 10), 0);
+}
+
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    frog.position.y -= 10;
+  }
+}
+
+
+function checkGameOver() {
+  if (frog.position.x <= 0 || width <= frog.position.x) {
+    fill(255, 0, 0);
+    textSize(60);
+    textAlign(CENTER);
+    text("GAME OVER", width/2, height/2);
+
+  }
+}
+
+
+function nextLevel() {
+  frog.position.x = width/2;
+  frog.position.y = height-30;
+}
+
+
+function playHitSound() {
+  sound_hit.play();
 }
